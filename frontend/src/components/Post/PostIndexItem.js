fd6b57f224bc86e9cafccbc0a337613457ca2cda
David <david.cabuay@gmail.com>
2023-03-16 16:07:27 -0700

    Update login forms

--- frontend/src/components/Post/PostIndexItem.js
+++ frontend/src/components/Post/PostIndexItem.js
@@ -3,20 +3,17 @@ import React from "react";
 import PostUpdateDeleteButtons from "./PostUpdateDeleteButtons";
 
 function PostIndexItem ({post}) {
-    
-
-
-
+    console.log(post);
     return (
         <>
             <Link to={`/posts/${post.id}`} >
-            <h1>Post content goes here</h1>
+            <h1>Post content goes here {post.id}</h1>
             <div>{post.url}</div>
             <p>{post.content}</p>
             {/* if post creator id equals the sessionUser id then show buttons where you can update or delete             */}
             </Link>
 
-            <PostUpdateDeleteButtons/>
+            <PostUpdateDeleteButtons post={post}/>
             
 
         </>
--- frontend/src/components/Post/PostUpdateDeleteButtons.js
+++ frontend/src/components/Post/PostUpdateDeleteButtons.js
@@ -12,7 +12,7 @@ function PostUpdateDeleteButtons ({post}) {
     const [deleteModalShow, setDeleteModalShow] = useState(false);
     const [updateModalShow, setUpdateShowModal] = useState(false);
     if (!sessionUser) return
-    if (post.authorId !== sessionUser.id) return
+    if (post.author !== sessionUser.id) return
 
     return (
         <div>
