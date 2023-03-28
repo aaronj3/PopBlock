a982cdf18df540c4b523e87954f3460f91458844
Aaron Jung <aaronjung3@gmail.com>
2023-03-28 19:22:42 -0400

    Move boxes around

--- frontend/src/components/Post/PostShow.js
+++ frontend/src/components/Post/PostShow.js
@@ -20,23 +20,17 @@ function PostShow(){
     const [animationStyle, setAnimationStyle] = useState({});
 
 
-
-    // const toggleAnimationClass = () => {
-    //     setAnimationClass(animationClass === '' ? 'roll' : '');
-    //   };
-      
-
       const handlePopClick = () => {
         setClicked(true);
         setAnimationStyle({ animation: 'roll 1s ease-in-out' });
         dispatch(likePost(post));
       };
-      
+
       const resetAnimation = () => {
         setAnimationStyle({});
       };
-      
-      
+
+
 
     const likes = useSelector(state => {
         return state.posts[postId]?.likes
@@ -68,38 +62,46 @@ function PostShow(){
         return (
             <>
                 <div className="showpage">
-                    <div className='content-container'>
-                    <h1>{post.content} by {post.author.username}</h1>
+
+                    <div className='media-container'>
+
                         {imgFlag ? (
                             <img src={post.url} height='500px' width='500px' alt=''/>
                         ) : (
                             <video src={post.url} controls width="500px"/>
                         )}
                     </div>
-                    <div>
+
+                    <div className='content-info-container'>
                         <br/>
+                        <h1>{post.content}</h1>
+                        <h2>{post.author.username}</h2>
+                        <img src="https://s.pinimg.com/webapp/love-c31e0b8d.svg"/>
+                        {/* <img src="https://s.pinimg.com/webapp/goodIdea-b770896d.svg"/> */}
+                        {/* <img src="https://s.pinimg.com/webapp/wow-3cb697ef.svg"/> */}
+                        <h2>{likes.length}</h2>
+
                         <div>
                             <PostUpdateDeleteButtons post={post}/>
-                            Pops: {likes.length} &nbsp;&nbsp;
-                            {(sessionUser) ?
-                           <button
-                           id="pop-button"
-                           style={animationStyle}
-                           onClick={handlePopClick}
-                           onAnimationEnd={resetAnimation}
-                         >
-                           {likes.includes(sessionUser._id) ? "UNPOP" : "POP"}
-                         </button>
-                         
-                            : ""}
+                                Pops: {likes.length} &nbsp;&nbsp;
+                                {(sessionUser) ?
+                                    <button
+                                    id="pop-button"
+                                    style={animationStyle}
+                                    onClick={handlePopClick}
+                                    onAnimationEnd={resetAnimation}>
+                                        <img src="https://s.pinimg.com/webapp/heartOutline-1f1b1ac2.svg"/>
+                                        {likes.includes(sessionUser._id) ? "UNPOP" : "POP"}
+                                    </button>
+                                : ""}
                         </div>
                         <br/>
                         <br/>
                     </div>
+                </div>
                 <div>
                     <Comments comments={comments}/>
-                {(sessionUser) ? <CreateCommentForm postId={postId}/> : <button className='requireLoginButton' onClick={()=>dispatch(showLoginModal())}>Log in to comment</button> }
-                </div>
+                    {(sessionUser) ? <CreateCommentForm postId={postId}/> : <button className='requireLoginButton' onClick={()=>dispatch(showLoginModal())}>Log in to comment</button> }
                 </div>
             </>
         );
