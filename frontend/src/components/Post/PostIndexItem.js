46b903d4f64fea9813bcbe4b740b86d4786bc90b
totorasora <k.sr.lee@gmail.com>
2023-03-29 00:23:11 -0700

    add style to post list show page

--- frontend/src/components/Post/PostIndexItem.js
+++ frontend/src/components/Post/PostIndexItem.js
@@ -5,8 +5,6 @@ import PostUpdateDeleteButtons from "./PostUpdateDeleteButtons";
 import './Post.css'
 
 function PostIndexItem ({post, index}) {
-    // console.log('post in index item', post);
-
     const isImage = (filename) =>{
         var ext = filename?.split('.').pop().toLowerCase();
         if (ext == 'jpg' || ext == 'jpeg' || ext == 'png' || ext == 'gif' || ext == 'bmp') {
@@ -14,49 +12,33 @@ function PostIndexItem ({post, index}) {
         }
         return false;
     }
+
     const sessionUser = useSelector(state => state.session.user);
-    const rank = index+1;
-    // console.log(index, "index");
-    // Styling idea - make the background color of the post container the color associated with that user
+    const rank = index + 1;
+
     if(!post){
         return null
-    }else{
+    } else{
         const imgFlag = isImage(post.url);
-    return (
-        <div className="post-index-item">
-            <Link to={`/posts/${post._id}`}>
-            <div className="showpage">
-                <div className="postContentContainer">
-                    <h1>#{rank} post by {post.author.username}</h1>
-                    {imgFlag ? (
-                        <img src={post.url} height='500px' width='500px' alt=''/>
-                    ) : (
-                        <video src={post.url} controls width="500px"/>
-                    )}
-                    <h2>{post.content}</h2>
-                    {sessionUser === post.author ? <PostUpdateDeleteButtons post={post}/> : <></>}
-                </div>
-
+        return (
+            <div className="post-index-item" style={{ backgroundColor: post.author.color }}>
+                <Link to={`/posts/${post._id}`}>
+                    <div className="showpage">
+                        <div className="postContentContainer">
+                            <h1 className="username">#{rank} {post.author.username}</h1>
+                            {imgFlag ? (
+                                <img src={post.url} alt='' className="post-media" />
+                            ) : (
+                                <video src={post.url} controls className="post-media" />
+                            )}
+                            <h2 className="post-likes">{post.likes.length} POPS</h2>
+                            <h2 className="post-content">{post.content}</h2>
+                        </div>
+                    </div>
+                </Link>
             </div>
-            </Link>
-        </div>
         )
     }
-
-    // return (
-    //     <>
-    //         <Link to={`/posts/${post._id}`} >
-    //         <h1>Post content goes here </h1>
-    //         <div>{post.url}</div>
-    //         <p>{post.content}</p>
-    //         {/* if post creator id equals the sessionUser id then show buttons where you can update or delete             */}
-    //         </Link>
-
-    //         <PostUpdateDeleteButtons post={post}/>
-
-
-    //     </>
-    //     )
 }
 
-export default PostIndexItem
+export default PostIndexItem;
